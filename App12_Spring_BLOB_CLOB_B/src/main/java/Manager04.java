4da6ebca6cc30e4467582e574de01b1f4e20d43d
amitkvaio <amitkvaio@gmail.com>
2023-02-11 00:34:33 +0530

    Adding image and sql queries
    Adding image and sql queries

--- App12_Spring_BLOB_CLOB_B/src/main/java/Manager04.java
+++ App12_Spring_BLOB_CLOB_B/src/main/java/Manager04.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,8 +10,10 @@ public class Manager04 {
 	public static void main(String[] args) throws IOException {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		StudentDAOImp studentDAOImp = (StudentDAOImp)ctx.getBean("studentJDBCTemplate");
-		byte[] image = studentDAOImp.getImage("D:\\Wallpaper\\windows.jpg");
-		studentDAOImp.updateImage(1, image);
+		byte[] image = studentDAOImp.getImage("D:\\git\\Spring-Core\\App12_Spring_BLOB_CLOB_B\\drawing.jpg");
+		
+		System.out.println("image::"+image.length);
+		studentDAOImp.updateImage(12, image);
 		System.out.println("Done");
 	}
 }
